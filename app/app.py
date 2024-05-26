ce6c69ef1d60cebd56d4d7e92c8f55f128300d73
HoodedFenrir <123753406+HoodedFenrir@users.noreply.github.com>
2024-05-25 21:41:58 -0600

    Update app.py
    Primer conecte con Login

--- app/app.py
+++ app/app.py
@@ -1,15 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = 'super_secret_key'  
 
+# Ruta de la página de introducción
 @app.route('/')
 def home():
-    return 'Restore HDMI'
+    return render_template('introduccion.html')
 
-@app.route('/submit')
-def recommend():
-  return 'hola'
+# Ruta de inicio de sesión
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        
+        return redirect(url_for('home'))  # Redirige a la página principal tras el inicio de sesión
+    return render_template('login.html')
+
+# Ruta de registro
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+      
+        return redirect(url_for('home'))  
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
